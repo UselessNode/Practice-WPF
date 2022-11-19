5accd7557dc00affc0f826dca593c8fb90218602
Koko <78096367+UselessNode@users.noreply.github.com>
2022-11-19 23:09:25 +0700

    Добавьте файлы проекта.

---
+++ Practice/View/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Practice.Database;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DatabaseEntities database = new DatabaseEntities();
+            ServicesList.ItemsSource = database.Services_.ToList();
+
+            LoadImages();
+        }
+
+        ObservableCollection<Services_> services { get; set; }
+
+        void LoadImages()
+        {
+            // Добавление?
+
+            //services = new ObservableCollection<Services_>
+            //{
+            //    new Services_ { Id = }
+            //}
+        }
+    }
+}
